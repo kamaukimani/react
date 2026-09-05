a64504809ce69675930899e33c3ead2fbdac6274
kamaukimani <paulkimys285@gmail.com>
2026-09-05 20:25:32 +0300

    render SpicyFoodList component

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import SpicyFoodList from "./SpicyFoodList"
+function App(){
+    return (
+        <div>
+            <h1>Hello there!!</h1>
+            <SpicyFoodList />
+        </div>
+    )
+}
+export default App
